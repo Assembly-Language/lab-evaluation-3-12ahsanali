4c51cccc1a2fd4cf4d8aa1333d7b60e04304ec13
12AhsanAli <ahsanmaqsood319@gmail.com>
2024-12-30 12:23:22 +0500

    run successfully

--- asm-file.asm
+++ asm-file.asm
@@ -1,4 +1,7 @@
-
+;AHSAN ALI
+;ROLL NO : 1130
+;Write a program that takes an array of numbers (with both positive and negative values) 
+;from the user (or declare ) and displays the total no. of  positive numbers from the array
 
 INCLUDE Irvine32.inc
 .data
@@ -9,19 +12,24 @@ msg db "assebly procedure end now",0
 .code
 asmfunc PROC p1:DWORD, p2:DWORD
     
-    push ebp
-    mov ebp,esp
-    mov eax, [ebp+12
-\] ; Move the first parameter into EAX
-    call writedec
-    call crlf
-    ;add eax, p2 ; Add the second parameter to EAX
-    call writedec
-    call crlf
-    mov edx, offset msg
-    call writestring
-    call crlf
-    
+   mov esi,p1
+   mov edi,p2
+
+   mov ecx,10
+
+ pass:
+    mov eax,[esi]
+    test eax, 8000h
+    jNz skip
+    mov eax,[edi]
+    add eax,1
+    mov [edi],eax
+
+
+skip:
+    add esi,4
+    loop pass
+
     ret
 asmfunc ENDP
 end
